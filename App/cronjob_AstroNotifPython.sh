ad0216866e34d76dab2278bf68bd4c1444e7faa1
PhilSubs <40604757+PhilSubs@users.noreply.github.com>
2018-06-29 15:38:18 +0200

    Ajout du path vers le répertoire App en paramètre du script

--- App/cronjob_AstroNotifPython.sh
+++ App/cronjob_AstroNotifPython.sh
@@ -1,2 +1,7 @@
-cd /var/www/html/cgi-bin
+#
+# usage : ./cronjob_AstroNotifPython.sh <path_to_App>
+#
+#  Extract the deployment.xml file from component tarball in obedeliv
+#
+cd $1
 python nightlyBatchBitmap.py
