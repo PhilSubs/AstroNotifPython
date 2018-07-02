bb78ae92654de88a3fcc5a3ab2ad48d9e5015d12
PhilSubs <40604757+PhilSubs@users.noreply.github.com>
2018-07-03 00:21:32 +0200

    Update cronjob_AstroNotifPython.sh

--- App/cronjob_AstroNotifPython.sh
+++ App/cronjob_AstroNotifPython.sh
@@ -1,8 +1,10 @@
+#!/bin/bash
 #
 # usage : ./cronjob_AstroNotifPython.sh
 #
 #  Generate HTML file and bitmaps
 #
 currentDir="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+echo Current dir is $currentDir
 cd $currentDir
 python nightlyBatchBitmap.py
