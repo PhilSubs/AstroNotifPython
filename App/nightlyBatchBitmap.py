b80d4a03d50a1a07e3f61ae297c36be10fb0cb1e
PhilSubs <40604757+PhilSubs@users.noreply.github.com>
2018-07-05 01:37:20 +0200

    Update nightlyBatchBitmap.py

--- App/nightlyBatchBitmap.py
+++ App/nightlyBatchBitmap.py
@@ -12,26 +12,26 @@ iNbLunarFeaturesobservable = 0
 iNbDeepSkyobjectsObservable = 0
 
 # Prepare objects for computing ephemeris
-print "Lecture des parametres du fichier parameters_run.json"
+print "Lecture des parametres du fichier parameters_run.json<br>"
 theParameters = ANLib.Parameters() # read parameters file parameters_run.json
 sParamDate = (datetime.now() + timedelta(hours=theParameters.getNightlyBatchTimeDeltaInHours())).strftime("%Y%m%d")  # start date from current date at 00:00 + delta hours
 theCalendar = ANLib.Calendar(sParamDate,"000000")
 theEphemeridesData = ANLib.EphemeridesData()
 
 # Compute ephemeris and produce new HTML
-print "Calcul des ephemerides"
+print "Calcul des ephemerides<br>"
 theEphemeridesData.computeEphemeridesForPeriod(theParameters, theCalendar)
-print "Generation de la page HTML et du bitmap"
+print "Generation de la page HTML et du bitmap<br>"
 theRendererBitmap = ANLib.RendererBitmap( theParameters.getGlobalPathToWWWFolder() + '/', "http://" + theParameters.getNightlyBatchDomain() + "/", True)
 sHTMLContent, iNbPlanetsObservable, iNbLunarFeaturesobservable, iNbDeepSkyobjectsObservable = theRendererBitmap.getHTML(theCalendar, theParameters, theEphemeridesData)
 
 # Save as default html file
 ANLib.Tools.saveAsFile(theParameters.getGlobalPathToWWWFolder() + '/' + theParameters.getNightlyBatchHTMLFilname(), sHTMLContent)
-print "La page HTML et le bitmap sont generes dans " + theParameters.getGlobalPathToWWWFolder() + '/'
+print "La page HTML et le bitmap sont generes dans " + theParameters.getGlobalPathToWWWFolder() + '/<br>'
 
 # Prepare email for daily notification
 if len(theParameters.getNightlyBatchEmailAddress()) > 0:
-    print "Preparation a l'envoi du mail"
+    print "Preparation a l'envoi du mail<br>"
     sTo = theParameters.getNightlyBatchEmailAddress()
     sSubject = "Ephemerides " + theCalendar.getFormattedDateForSlot(0, 0)
     if (iNbPlanetsObservable + iNbLunarFeaturesobservable + iNbDeepSkyobjectsObservable) > 0:
@@ -48,5 +48,5 @@ if len(theParameters.getNightlyBatchEmailAddress()) > 0:
             if iNbDeepSkyobjectsObservable> 1: sSubject = sSubject + "s"
 
     # Send email
-    print "Envoi du mail"
+    print "Envoi du mail<br>"
     ANLib.Tools.sendEmailHTML(theParameters.getNightlyBatchEmailFromAdress(), sTo, sSubject, sHTMLContent, theParameters.getNightlyBatchEmailSMTPServer() )
