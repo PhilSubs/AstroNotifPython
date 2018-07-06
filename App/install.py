b806a9781537a0d7ade76658e4724d4ecc667350
PhilSubs <40604757+PhilSubs@users.noreply.github.com>
2018-07-06 08:50:25 +0200

    Update install.py

--- App/install.py
+++ App/install.py
@@ -1,3 +1,10 @@
+#!/usr/bin/python2.7
+# -*-coding:Latin-1 -*
+
+# Import libs for ephemerides
+from datetime import datetime, timedelta
+import ANLib
+
 # Update des fichiers JSON si existant
   # Affichage des changement
 # Affichage des instructions pour le chmod
