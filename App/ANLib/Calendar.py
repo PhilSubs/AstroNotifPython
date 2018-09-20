c7b5a103eb9cfac5712b476d7961b88a92ae0632
PhilSubs <40604757+PhilSubs@users.noreply.github.com>
2018-09-20 16:14:55 +0200

    Update Calendar.py

--- App/ANLib/Calendar.py
+++ App/ANLib/Calendar.py
@@ -9,18 +9,20 @@ import datetime
 from MeeusAlgorithmsFormulas import MeeusAlgorithmsFormulas
 
 class Calendar(toolObjectSerializable):
-    def __init__(self, sDate, sTime):
+    def __init__(self, sDate, sTime, fLocalTimeDifferenceWithGMT):
         toolObjectSerializable.__init__(self)
-        self._date = sDate # date as YYYYMMDD
-        self._time = sTime # Time as HHMMSS (GMT)
+        self._date = sDate # date as YYYYMMDD  (Local Time)
+        self._time = sTime # Time as HHMMSS    (Local Time)
+        self._fLocalTimeDifferenceWithGMT = fLocalTimeDifferenceWithGMT
         self._iYear = int(sDate[0:4])
         self._iMonth = int(sDate[4:6])
         self._iDay = int(sDate[6:8])
         self._iHours = int(sTime[0:2])
         self._iMinutes = int(sTime[2:4])
         self._iSeconds = int(sTime[4:8])
-        self._dtDateTime =  datetime.datetime(int(sDate[0:4]),int(sDate[4:6]),int(sDate[6:8]),int(sTime[0:2]),int(sTime[2:4]),int(sTime[4:8]))
-        self._fDateValue = self.__computeDateValueFromDateAndTime(sDate, sTime)
+        self._dtLocalDateTime =  datetime.datetime(int(sDate[0:4]),int(sDate[4:6]),int(sDate[6:8]),int(sTime[0:2]),int(sTime[2:4]),int(sTime[4:8]))
+        self._dtGMTLocalDateTime =  self._dtLocalDateTime - datetime.timedelta(hours=self._fLocalTimeDifferenceWithGMT)
+        self._fLocalDateValue = self.__computeDateValueFromDateAndTime(sDate, sTime)
     def __computeDateValueFromDateAndTime(self, sDate, sTime):
         # Compute the number of days since 01/01/2000 at 00:00 which is the reference date
         # Split date and time 
@@ -29,21 +31,26 @@ class Calendar(toolObjectSerializable):
         #fDateValue = fDateValue # - 0.5
         fDateValue = self.getJulianDate() - MeeusAlgorithmsFormulas.JulianDay_07_01(2000, 1, 1, 0, 0, 0)
         return fDateValue
-    def getDate(self): return self._date
+    def getLocalDate(self): return self._date
+    def getGMTDate(self): return self._date
     def getJulianDate(self):
-        return MeeusAlgorithmsFormulas.JulianDay_07_01(self._iYear, self._iMonth, self._iDay, self._iHours, self._iMinutes, self._iSeconds)
-    def getDateForSlot(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%Y%m%d')
-    def getFormattedDateForSlot(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%d %b %Y')
+        return MeeusAlgorithmsFormulas.JulianDay_07_01(self._dtGMTLocalDateTime.year, self._dtGMTLocalDateTime.month, self._dtGMTLocalDateTime.day, self._dtGMTLocalDateTime.hour, self._dtGMTLocalDateTime.minute, self._dtGMTLocalDateTime.second)
+    def getLocalDateForSlot(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtLocalDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%Y%m%d')
+    def getGMTDateForSlot(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtGMTLocalDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%Y%m%d')
+    def getFormattedLocalDateForSlot(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtLocalDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%d %b %Y')
+    def getFormattedGMTDateForSlot(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtGMTLocalDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%d %b %Y')
     def getTime(self): return self._time
-    def getLocalTimeForSlot(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%H%M%S')  #TODO: compute local time based on DST and area
-    def getTimeForSlot(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%H%M%S')
-    def getLocalTimeForSlotAsHHMM(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%H:%M')  #TODO: compute local time based on DST and area
-    def getTimeForSlotAsHHMM(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%H:%M')
+    def getLocalTimeForSlot(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtLocalDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%H%M%S') 
+    def getGMTTimeForSlot(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtGMTLocalDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%H%M%S')
+    def getLocalTimeForSlotAsHHMM(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtLocalDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%H:%M')  #TODO: compute local time based on DST and area
+    def getGMTTimeForSlotAsHHMM(self, iSlot, iNumberOfMinutesPerSlot): return (self._dtGMTLocalDateTime + datetime.timedelta(minutes=(iNumberOfMinutesPerSlot * iSlot))).strftime('%H:%M')
     def getNumberOfDaysSinceRef(self): return self._fDateValue
-    def getDateValueForTimeSlot(self, iTimeSlot, iNumberOfMinutesPerSlot):
-        # compute initial DateValue for 00:00:00
-        fDateValue = self.__computeDateValueFromDateAndTime(self._date  , "000000")
+    def getLocalDateValueForTimeSlot(self, iTimeSlot, iNumberOfMinutesPerSlot):
         # add delta corresponding to timeslot
-        fDateValue = fDateValue + (float(iNumberOfMinutesPerSlot) / 1440.0 * float(iTimeSlot))
+        fDateValue = self._fLocalDateValue + (float(iNumberOfMinutesPerSlot) / 1440.0 * float(iTimeSlot))
+        return fDateValue
+    def getGMTDateValueForTimeSlot(self, iTimeSlot, iNumberOfMinutesPerSlot):
+        # add delta corresponding to timeslot
+        fDateValue = self._fLocalDateValue + (float(iNumberOfMinutesPerSlot) / 1440.0 * float(iTimeSlot)) - (fLocalTimeDifferenceWithGMT / 24.0)
         return fDateValue
     
