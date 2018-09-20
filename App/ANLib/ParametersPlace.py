2a0d2a9ab51630ea4acd03dc67e6d3a831a62d0c
PhilSubs <40604757+PhilSubs@users.noreply.github.com>
2018-09-20 15:37:16 +0200

    Update ParametersPlace.py

--- App/ANLib/ParametersPlace.py
+++ App/ANLib/ParametersPlace.py
@@ -10,14 +10,17 @@ from ParametersObstructedSkyAreas import ParametersObstructedSkyAreas
 
 
 class ParametersPlace(toolObjectSerializable):
-    def __init__(self, sName, fLongitude, fLatitude):
+    def __init__(self, sName, fLocalTimeDifferenceWithGMT, fLongitude, fLatitude):
         toolObjectSerializable.__init__(self)
         self._sName = sName
+        self._fLocalTimeDifferenceWithGMT = fLocalTimeDifferenceWithGMT
         self._fLongitude = fLongitude
         self._fLatitude = fLatitude
         self._theObstructedSkyAreas = None
     def getName(self): return self._sName
     def setName(self, sName): self._sName = sName
+    def getLocalTimeDifferenceWithGMT(self): return self._fLocalTimeDifferenceWithGMT
+    def setLocalTimeDifferenceWithGMT(self, fLocalTimeDifferenceWithGMT): self._fLocalTimeDifferenceWithGMT = fLocalTimeDifferenceWithGMT
     def getLongitude(self): return self._fLongitude
     def setLongitude(self, fLongitude): self._fLongitude = fLongitude
     def getLatitude(self): return self._fLatitude
