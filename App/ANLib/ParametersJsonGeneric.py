1286be23d7d0c3feb65cb073991bd387c4cc3389
PhilSubs <40604757+PhilSubs@users.noreply.github.com>
2018-10-10 16:28:16 +0200

    Update ParametersJsonGeneric.py

--- App/ANLib/ParametersJsonGeneric.py
+++ App/ANLib/ParametersJsonGeneric.py
@@ -7,9 +7,10 @@ from toolJSON import toolJSON
 
 class ParametersJsonGeneric:
     def __init__(self, sJsonFileName):
-        self._dicJSONData = toolJSON.getContent(sJsonFileName)
+        self._jsonFileName = sJsonFileName
+        self._dicJSONData = toolJSON.getContent(self._jsonFileName)
         
-    def get(self, sParameterName):
+    def get(self, sParameterName, bNotifyMissing = True):
         # Parameter name has following form :  param1.param2.param3...
         # Compute path from parameter name in order to have something like: ["param1"]["param2"]["param3"
         arrPath = sParameterName.split(".")
@@ -19,10 +20,11 @@ class ParametersJsonGeneric:
         try:
             dicParameter = eval("self._dicJSONData" + sDicPath)
         except:
-            print ""
-            print "   >>> ERROR >>> get json parameter '" + sParameterName + "' from  file  'exemple_parameters.json'  FAILED !!"
-            print "                 Unknown parameter !!"
-            print ""
+            if bNotifyMissing:
+                print ""
+                print "   >>> ERROR >>> get json parameter '" + sParameterName + "' from  file  '" + self._jsonFileName + "'  FAILED !!"
+                print "                 Unknown parameter !!"
+                print ""
             raise
         # get the parameter type from the JSON file content
         sParameterType = "unknown"
@@ -32,10 +34,11 @@ class ParametersJsonGeneric:
             if isinstance(dicParameter, dict):
                 sParameterType = "dict"
             else:
-                print ""
-                print "   >>> ERROR >>> get json parameter '" + sParameterName + "' from  file  'exemple_parameters.json'  FAILED !!"
-                print "                 Can't find parameter type !!"
-                print ""
+                if bNotifyMissing:
+                    print ""
+                    print "   >>> ERROR >>> get json parameter '" + sParameterName + "' from  file  '" + self._jsonFileName + "'  FAILED !!"
+                    print "                 Can't find parameter type !!"
+                    print ""
                 raise
         #Compute the return value depending on the parameter type
         sReturn = None
@@ -53,10 +56,11 @@ class ParametersJsonGeneric:
             elif sParameterType == "dict":
                 sReturn = dicParameter
         except:
+            if bNotifyMissing:
                 print ""
-                print "   >>> ERROR >>> get json parameter '" + sParameterName + "' from  file  'exemple_parameters.json'  FAILED !!"
+                print "   >>> ERROR >>> get json parameter '" + sParameterName + "' from  file  '" + self._jsonFileName + "'  FAILED !!"
                 print "                 Can't find parameter value !!"
                 print ""
-                raise
+            raise
         # return the parameter value as string, int, float, tuple, boolean or dict
         return sReturn
